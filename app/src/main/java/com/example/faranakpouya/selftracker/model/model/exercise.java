5c27635536ffd999c0ab8687429a5205c29e6284
fnobakht@sfu.ca <frnknb@gmailcom>
2018-02-10 14:31:41 -0800

    Create user model package and user class

---
+++ app/src/main/java/com/example/faranakpouya/selftracker/model/model/exercise.java
@@ -0,0 +1,8 @@
+package com.example.faranakpouya.selftracker.model.model;
+
+/**
+ * Created by faranakpouya on 2018-02-10.
+ */
+
+public class exercise {
+}
---
+++ app/src/main/java/com/example/faranakpouya/selftracker/model/model/user.java
@@ -0,0 +1,11 @@
+package com.example.faranakpouya.selftracker.model.model;
+
+/**
+ * Created by faranakpouya on 2018-02-10.
+ */
+
+public class user {
+    private int glass_of_water;
+
+
+}
