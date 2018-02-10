38f3b89eed4545426f0b26a64f406d6b0568f128
fnobakht@sfu.ca <frnknb@gmailcom>
2018-02-10 13:52:15 -0800

    Create water and study activity

--- app/src/main/java/com/example/faranakpouya/selftracker/model/goals.java
+++ app/src/main/java/com/example/faranakpouya/selftracker/model/goals.java
@@ -2,6 +2,8 @@ package com.example.faranakpouya.selftracker.model;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.faranakpouya.selftracker.R;
 
@@ -11,5 +13,23 @@ public class goals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
+    }
+
+    private void createWaterButton(){
+        Button waterBtn = (Button) findViewById(R.id.btn_water);
+        waterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void createStudyButton(){
+
+    }
+
+    private void createExerciseButton(){
+
     }
 }
---
+++ app/src/main/java/com/example/faranakpouya/selftracker/model/water.java
@@ -0,0 +1,15 @@
+package com.example.faranakpouya.selftracker.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.faranakpouya.selftracker.R;
+
+public class water extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water);
+    }
+}
